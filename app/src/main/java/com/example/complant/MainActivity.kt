9de6c161c30737e6e647d1c58e4f1bae62c5c07c
Choi-JungWon <0401ah@gmail.com>
2021-05-16 05:24:56 +0900

    사진 업로드 액티비티와 다이어리 프래그먼트 만들기 전

--- app/src/main/java/com/example/complant/MainActivity.kt
+++ app/src/main/java/com/example/complant/MainActivity.kt
@@ -3,48 +3,82 @@ package com.example.complant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.example.complant.navigation.AlarmFragment
-import com.example.complant.navigation.BoardFragment
-import com.example.complant.navigation.DictionaryFragment
-import com.example.complant.navigation.HomeFragment
+import android.view.View
+import androidx.core.app.ActivityCompat
+import com.example.complant.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 로딩 바를 작동
+        progress_bar.visibility = View.VISIBLE
+
+        // Bottom Navigation View
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        // 페이지 시작 시 R.id.action_home 버튼이 작동되도록 설정
+        bottom_navigation.selectedItemId = R.id.action_home
+
+        //스토리지 접근 권한 요청
+//        ActivityCompat.requestPermissions(this,
+//        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+    }
+
+    fun setToolbarDefault() {
+        toolbar_title_image.visibility = View.VISIBLE
+        toolbar_btn_back.visibility = View.GONE
+        toolbar_username.visibility = View.GONE
+    }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        setToolbarDefault()
         when(item.itemId) {
             R.id.action_board -> {
-                var BoardFragment = BoardFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, BoardFragment).commit()
+                var boardFragment = BoardFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, boardFragment)
+                    .commit()
                 return true
             }
             R.id.action_dictionary -> {
-                var DictionaryFragment = DictionaryFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, DictionaryFragment).commit()
+                var dictionaryFragment = DictionaryFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, dictionaryFragment)
+                    .commit()
                 return true
             }
             R.id.action_home -> {
-                var HomeFragment = HomeFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, HomeFragment).commit()
+                var homeFragment = HomeFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, homeFragment)
+                    .commit()
                 return true
             }
             R.id.action_alarm -> {
-                var AlarmFragment = AlarmFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, AlarmFragment).commit()
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, alarmFragment)
+                    .commit()
                 return true
             }
             R.id.action_diary -> {
-                var DictionaryFragment = DictionaryFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content, DictionaryFragment).commit()
+                var diaryFragment = DiaryFragment()
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_content, diaryFragment)
+                    .commit()
                 return true
             }
         }
         return false
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        bottom_navigation.setOnNavigationItemSelectedListener(this)
-    }
 }
--- app/src/main/java/com/example/complant/navigation/DiaryFragment.kt
+++ app/src/main/java/com/example/complant/navigation/DiaryFragment.kt
@@ -1,11 +1,15 @@
 package com.example.complant.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.complant.MainActivity
 import com.example.complant.R
+
 
 class DiaryFragment : Fragment() {
     override fun onCreateView(
@@ -17,3 +21,6 @@ class DiaryFragment : Fragment() {
         return view
     }
 }
+
+
+
