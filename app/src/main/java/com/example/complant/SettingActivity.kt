b9043c868eef881cc5ba82f69c8568a7d4d4fab7
hyee0715 <hyee0715@naver.com>
2021-05-24 11:43:56 +0900

    SettingActivity.kt 수정

--- app/src/main/java/com/example/complant/SettingActivity.kt
+++ app/src/main/java/com/example/complant/SettingActivity.kt
@@ -2,10 +2,34 @@ package com.example.complant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        // 1. ID / PW 변경
+        btn_img_id_pw_setting.setOnClickListener {  }
+        btn_id_pw_setting.setOnClickListener {  }
+
+        // 2. 회원 탈퇴
+        btn_img_quit_setting.setOnClickListener {  }
+        btn_quit_setting.setOnClickListener {  }
+
+        // 3. 메인 메시지 설정
+        btn_img_main_message_setting.setOnClickListener {  }
+        btn_main_message_setting.setOnClickListener {  }
+
+        // 4. 프로필 사진 변경
+        btn_img_profile_image_setting.setOnClickListener {  }
+        btn_profile_image_setting.setOnClickListener {  }
+
+        // 5. 반려식물 별칭 변경
+        btn_img_plant_name_setting.setOnClickListener {  }
+        btn_img_plant_name_setting.setOnClickListener {  }
+
     }
+
+
 }
