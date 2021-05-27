e88f3b1c253f00458a13bb3954f105ec426f59b4
hyee0715 <hyee0715@naver.com>
2021-05-28 02:57:35 +0900

    MessageListFragment.kt Timestamp로 인해 자동 생성되는 null 데이터 오류 해결

--- app/src/main/java/com/example/complant/navigation/MessageListFragment.kt
+++ app/src/main/java/com/example/complant/navigation/MessageListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.complant.navigation
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -12,20 +13,21 @@ import com.example.complant.MainActivity
 import com.example.complant.R
 import com.example.complant.navigation.model.ContentDTO
 import com.example.complant.navigation.model.MessageDTO
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
 import kotlinx.android.synthetic.main.fragment_diary.view.*
 import kotlinx.android.synthetic.main.fragment_message_list.*
 import kotlinx.android.synthetic.main.fragment_message_list.view.*
 import kotlinx.android.synthetic.main.item_message.view.*
+import kotlin.collections.ArrayList
 
 class MessageListFragment : Fragment() {
     var mainActivity: MainActivity? = null
     var firestore : FirebaseFirestore? = null
-
-    var str1 : String? = null
-    var str2 : String? = null
-    var str3 : String? = null
-    var str4 : String? = null
+    var auth : FirebaseAuth? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -44,23 +46,37 @@ class MessageListFragment : Fragment() {
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_message_list, container, false)
         firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        var messageInfo = MessageDTO()
+
 
         view.btn_new_message?.setOnClickListener {
             mainActivity?.goMessageSettingFragment()
+
         }
 
-        str1 = arguments?.getString("message_date")
-        str2 = arguments?.getString("message_start_time")
-        str3 = arguments?.getString("message_end_time")
-        str4 = arguments?.getString("message_contents_input")
 
-        view.date.text = str1
-        view.start.text = str2
-        view.end.text = str3
-        view.contentssss.text = str4
+        var str1 : String? = arguments?.getString("message_date")
+        var str2 : String? = arguments?.getString("message_start_time")
+        var str3 : String? = arguments?.getString("message_end_time")
+        var str4 : String? = arguments?.getString("message_contents_input")
+
+        //if (true) {
 
 
 
+            messageInfo.date = str1
+            messageInfo.startTime = str2
+            messageInfo.endTime = str3
+            messageInfo.content = str4
+            messageInfo.timestamp = System.currentTimeMillis()
+
+            if (messageInfo.date != null && messageInfo.startTime != null && messageInfo.endTime != null && messageInfo.content != null) {
+                firestore?.collection("messages")?.document(messageInfo?.timestamp.toString())?.set(messageInfo)
+
+            }
+
+       // }
 
         view.fragment_message_recyclerview.adapter = MessageListRecyclerViewAdapter()
         view.fragment_message_recyclerview.layoutManager = LinearLayoutManager(activity)
@@ -73,9 +89,7 @@ class MessageListFragment : Fragment() {
         var messageDTOs : ArrayList<MessageDTO> = arrayListOf()
 
         init {
-            // firestore?.collection("messages")?.orderBy("timestamp")
-            //                ?.addSnapshotListener { querySnapshot,  firebaseFirestoreException ->
-            firestore?.collection("messages")?.addSnapshotListener { querySnapshot,  firebaseFirestoreException ->
+            firestore?.collection("messages")?.orderBy("timestamp", Query.Direction.DESCENDING)?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
                     // 배열 비움
                     messageDTOs.clear()
 
@@ -113,4 +127,6 @@ class MessageListFragment : Fragment() {
 
         }
     }
+
+
 }
